cb7e93f0db641754d6103a3873bc100c90da2994
Yuichiro Nakamine <e185726@ie.u-ryukyu.ac.jp>
2019-02-14 15:48:45 +0900

    セカンド

---
+++ report6/src/jp/ac/uryukyu/ie/e185726/Result.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e185726;
+
+public class Result {
+    private static void winlose() {
+        int win = 0;
+        int lose = 0;
+
+        System.out.println("あなたの勝敗数");
+        System.out.println("勝ち：" + win + "回");
+        System.out.println("負け：" + lose + "回");
+
+    }
+
+
+}
